b2ca1728132d2b012f85f64dccf447135db5a55a
Aditya Byju <byjuaditya@gmail.com>
2024-07-31 11:49:44 -0500

    Update static

--- salesight/settings.py
+++ salesight/settings.py
@@ -155,6 +155,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
