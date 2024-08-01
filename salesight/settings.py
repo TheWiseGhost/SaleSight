88936304225f06af3ad092de8514bb83b5fdc73d
Aditya Byju <byjuaditya@gmail.com>
2024-08-01 08:19:06 -0500

    make migrations

--- salesight/settings.py
+++ salesight/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/src/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
