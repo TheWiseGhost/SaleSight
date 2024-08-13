a5686e9d81f5fc7c29b4b786ccec39e057ed10ed
Aditya Byju <byjuaditya@gmail.com>
2024-08-12 21:43:18 -0500

    work

--- salesight/settings.py
+++ salesight/settings.py
@@ -83,7 +83,16 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://salesight.netlify.app'
-]   
+]
+
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
 
 ROOT_URLCONF = 'salesight.urls'
 
